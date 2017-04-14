983b1593df0926933f1391f8162f4e00540a09b8
Gian Singh Sarao <AwesomeOrange1000@gmail.com>
2017-04-14 11:17:37 +0100

    Updated 404 To New Framework

--- js/error.js
+++ js/error.js
@@ -1,11 +1,11 @@
 requirejs.config({
-    baseUrl: 'js/'
+    baseUrl: '/js/'
 });
 (function(window, document, requirejs){
 requirejs(['_'], function(){
   componentHandler.upgradeDom();
   document.querySelector('#HomeBTN').addEventListener('click', function(){
-    window.location = "../";
+    window.location = "/";
   });
 });
 })(window, document, requirejs);
